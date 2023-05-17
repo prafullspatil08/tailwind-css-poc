3cef42b00122fb2004a008ab2619927be33188e5
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-05-17 17:56:33 +0530

    managed auth routes

--- src/app/feature/auth/auth-routing.module.ts
+++ src/app/feature/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+      { path: 'sign-in', component: SignInComponent, data: { returnUrl: window.location.pathname } },
+      { path: 'sign-up', component: SignUpComponent },
+      { path: '**', redirectTo: 'sign-in', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    HttpClientModule,
+    AngularSvgIconModule.forRoot(),
   ]
 })
 export class AuthModule { }
