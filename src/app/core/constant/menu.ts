2e0fa471d61d133baf782717e12f00090c3d8bf0
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-05-17 15:43:58 +0530

    removed unused code

--- src/app/core/constant/menu.ts
+++ src/app/core/constant/menu.ts
@@ -11,7 +11,7 @@ export class Menu {
           icon: 'assets/icons/outline/chart-pie.svg',
           label: 'Dashboard',
           route: '/dashboard',
-          children: [{ label: 'Nfts', route: '/dashboard/nfts' }],
+          children: [{ label: 'posts', route: '/dashboard/nfts' }],
         },
         {
           icon: 'assets/icons/outline/lock-closed.svg',
@@ -20,31 +20,7 @@ export class Menu {
           children: [
             { label: 'Sign up', route: '/auth/sign-up' },
             { label: 'Sign in', route: '/auth/sign-in' },
-            { label: 'Forgot Password', route: '/auth/forgot-password' },
-            { label: 'New Password', route: '/auth/new-password' },
-            { label: 'Two Steps', route: '/auth/two-steps' },
           ],
-        },
-      ],
-    },
-    {
-      group: 'Collaboration',
-      separator: true,
-      items: [
-        {
-          icon: 'assets/icons/outline/download.svg',
-          label: 'Download',
-          route: '/download',
-        },
-        {
-          icon: 'assets/icons/outline/gift.svg',
-          label: 'Gift Card',
-          route: '/gift',
-        },
-        {
-          icon: 'assets/icons/outline/users.svg',
-          label: 'Users',
-          route: '/users',
         },
       ],
     },
@@ -57,21 +33,7 @@ export class Menu {
           label: 'Settings',
           route: '/settings',
         },
-        {
-          icon: 'assets/icons/outline/bell.svg',
-          label: 'Notifications',
-          route: '/gift',
-        },
-        {
-          icon: 'assets/icons/outline/folder.svg',
-          label: 'Folders',
-          route: '/folders',
-          children: [
-            { label: 'Current Files', route: '/folders/current-files' },
-            { label: 'Downloads', route: '/folders/download' },
-            { label: 'Trash', route: '/folders/trash' },
-          ],
-        },
+      
       ],
     },
   ];
