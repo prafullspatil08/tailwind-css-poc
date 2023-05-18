73333648cd1ae627107b7fde0f2b69d0a770ce39
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-05-18 11:40:32 +0530

    sign-up functionality

--- src/app/feature/auth/sign-up/sign-up.component.ts
+++ src/app/feature/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  signUpForm!:FormGroup  ;
+  passwordTextType!: boolean;
+  submitted = false;
+  constructor(private _fb: FormBuilder, private router: Router, private api: ApiService) {}
+  
+  ngOnInit(): void {
+    this.signUpForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      phone_no: ['',Validators.required]
+    });
+  }
 
+  get form() {
+    return this.signUpForm.controls;
+  }
+
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  onSubmit(){
+    this.submitted = true
+    if(this.signUpForm.status == 'VALID'){
+      this.api.signUp(this.signUpForm?.value).subscribe((res)=> {
+        alert("Register Successfully");
+        this.signUpForm.reset({});
+        this.router.navigate(['/auth/sign-in']);
+      }, (error:any)=>{
+        alert("Internal Server Error")
+      });
+    }
+  }
 }
